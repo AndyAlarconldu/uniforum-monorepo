5fca88ca07539aa95e773a72cbc03f49664e998e
AndyAlarconldu <andyjavier301067@gmail.com>
2026-01-06 14:28:31 -0500

    feat: :zap: add services

---
+++ backend/services/user-service/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="User Service",
+    version="1.0.0"
+)
+
+@app.get("/")
+def health():
+    return {"status": "User Service running"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {
+        "id": user_id,
+        "name": "Demo User",
+        "email": "demo@uniforum.edu"
+    }
