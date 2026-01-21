e7db734abf4dcf38d28b63c4b8c18ca8e9f35113
AndyAlarconldu <andyjavier301067@gmail.com>
2026-01-21 01:37:58 -0500

    feat: :art: inserting code into module-security files

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -1,7 +1,5 @@
-resource "aws_security_group" "alb" {
-  name        = "${var.project_name}-${var.env}-sg-alb"
-  description = "ALB SG"
-  vpc_id      = var.vpc_id
+resource "aws_security_group" "app" {
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 80
@@ -10,89 +8,11 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "bastion" {
-  name   = "${var.project_name}-${var.env}-sg-bastion"
-  vpc_id = var.vpc_id
-
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "app" {
-  name   = "${var.project_name}-${var.env}-sg-app"
-  vpc_id = var.vpc_id
-
-  # tráfico desde ALB hacia la app (80)
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
-
-  # SSH desde bastion
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "rds" {
-  name   = "${var.project_name}-${var.env}-sg-rds"
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "redis" {
-  name   = "${var.project_name}-${var.env}-sg-redis"
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port       = 6379
-    to_port         = 6379
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app.id]
   }
 
   egress {
--- terraform/modules/security/outputs.tf
+++ terraform/modules/security/outputs.tf
@@ -1,19 +1,3 @@
-output "sg_alb_id" {
-  value = aws_security_group.alb.id
-}
-
-output "sg_bastion_id" {
-  value = aws_security_group.bastion.id
-}
-
-output "sg_app_id" {
+output "app_sg_id" {
   value = aws_security_group.app.id
-}
-
-output "sg_rds_id" {
-  value = aws_security_group.rds.id
-}
-
-output "sg_redis_id" {
-  value = aws_security_group.redis.id
 }
