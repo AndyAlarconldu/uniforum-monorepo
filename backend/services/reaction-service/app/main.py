1362bc60e4decc067a5981f987afabaf4771411f
AndyAlarconldu <andyjavier301067@gmail.com>
2026-01-23 02:45:06 -0500

    feat: :art: add microservise reaction-service

--- backend/services/reaction-service/app/main.py
+++ backend/services/reaction-service/app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from app.database import redis_client
 
-app = FastAPI(title="Reply Service")
+app = FastAPI(title="Reaction Service")
 
-@app.get("/")
+@app.get("/health")
 def health():
-    return {"status": "Reply Service running"}
+    return {"status": "ok"}
 
-@app.post("/replies")
-def reply():
-    return {"message": "Reply created"}
+@app.post("/reactions/{post_id}/like")
+def like_post(post_id: int):
+    key = f"post:{post_id}:likes"
+    likes = redis_client.incr(key)
+    return {"post_id": post_id, "likes": likes}
+
+@app.get("/reactions/{post_id}")
+def get_likes(post_id: int):
+    key = f"post:{post_id}:likes"
+    likes = redis_client.get(key)
+    return {"post_id": post_id, "likes": int(likes) if likes else 0}
